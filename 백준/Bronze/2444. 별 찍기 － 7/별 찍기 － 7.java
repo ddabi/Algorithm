6713801f969ca500cf56a78d172f728a1e8905b6
유다빈 <115603720+ddabi@users.noreply.github.com>
2023-12-04 17:51:13 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 516 ms, Memory: 23724 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2 * n - 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
